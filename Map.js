68995832b1a8dff1fdc359ec554f2804eb0b1e69
simon.gon.mon <simon.gon.mon@gmail.com>
2024-03-31 13:49:21 -0500

    Update Map.js

--- Map.js
+++ Map.js
@@ -1,56 +1,83 @@
-import React from "react";
-import { MapContainer, ImageOverlay, Rectangle } from "react-leaflet";
+import React, { useRef, useState } from "react";
+import { MapContainer, ImageOverlay, Polygon } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-
-// [arriba, derecha]
 
 function MapComponent() {
   const imageUrl =
     "https://thumb.bibliocad.com/images/content/00020000/5000/25945.jpg";
-  const finalXCoord = 1000;
-  const finalYCoord = 750;
-  const imageBounds = [
-    [0, 0],
-    [finalYCoord, finalXCoord],
-  ];
+  const imageRef = useRef(null);
+  const [imageWidth, setImageWidth] = useState(0);
+  const [imageHeight, setImageHeight] = useState(0);
 
   const zones = [
-    [
-      [finalYCoord - 254, 679],
-      [finalYCoord - 146, 877],
-    ],
+    {
+      name: "Zona 1",
+      coords: [
+        [254, 679],
+        [254, 877],
+        [146, 877],
+        [146, 679],
+        [254, 679],
+      ],
+    },
   ];
 
-  const onClickedZone = () => {
-    alert("Rectangle clicked!");
+  const onClickedZone = (zoneName) => {
+    alert(`Clicked on zone: ${zoneName}`);
   };
 
+  const handleImageLoad = () => {
+    const width = imageRef.current.naturalWidth;
+    const height = imageRef.current.naturalHeight;
+    setImageWidth(width);
+    setImageHeight(height);
+  };
+
+  const imageCRS = L.CRS.Simple;
+
+  const maxBounds = [[0, 0], [imageHeight, imageWidth]];
+
+  const finalZones = zones.map((zone) => ({
+    name: zone.name,
+    coords: zone.coords.map(([y, x]) => [imageHeight - y, x]),
+  }));
+
   return (
-    <MapContainer
-      crs={L.CRS.Simple}
-      center={[finalYCoord / 2, finalXCoord / 2]}
-      zoom={0}
-      maxZoom={1}
-      style={{ height: "100%", width: "100%", backgroundColor: "black" }}
-      attributionControl={false}
-    >
-      <ImageOverlay url={imageUrl} bounds={imageBounds} />
-      {zones.map((zone, index) => {
-        return (
-          <ImageOverlay
-            key={index}
-            url={
-              "https://www.clker.com/cliparts/9/9/c/8/q/7/grey-rounded-rectangle-gery-rounded-rectangle.svg.hi.png"
-            }
-            opacity={0.5}
-            bounds={zone}
-            interactive={true}
-            eventHandlers={{ click: onClickedZone }}
-          />
-        );
-      })}
-    </MapContainer>
+    <>
+      <img
+        ref={imageRef}
+        src={imageUrl}
+        alt="Map"
+        style={{ display: "none" }}
+        onLoad={handleImageLoad}
+      />
+      {imageWidth > 0 && imageHeight > 0 && (
+        <MapContainer
+          crs={imageCRS}
+          center={[imageHeight / 2, imageWidth / 2]}
+          zoom={0}
+          minZoom={0}
+          maxZoom={1}
+          maxBounds={maxBounds}
+          style={{ height: "100%", width: "100%", backgroundColor: "black" }}
+          attributionControl={false}
+        >
+          <ImageOverlay url={imageUrl} bounds={maxBounds} />
+          {finalZones.map(({ name, coords }, index) => {
+            return (
+              <Polygon
+                key={index}
+                positions={coords}
+                pathOptions={{ color: "white", fillColor: "white", fillOpacity: 0.5, weight: 0 }}
+                interactive={true}
+                eventHandlers={{ click: () => onClickedZone(name) }}
+              />
+            );
+          })}
+        </MapContainer>
+      )}
+    </>
   );
 }
 

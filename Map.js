ed1cf27cb7d17cfbff6c80f2330b39efd5b98774
simon.gon.mon <simon.gon.mon@gmail.com>
2024-03-31 18:07:27 -0500

    .

--- Map.js
+++ Map.js
@@ -1,16 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { MapContainer, ImageOverlay, Polygon, Circle } from "react-leaflet";
+import { MapContainer, ImageOverlay, Polygon, Circle, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
 import { Image } from "react-native";
+import { FaDrawPolygon, FaRegSquare, FaRegCircle } from "react-icons/fa";
 
-// [arriba, x de izq a der]
+const DrawControls = ({ onCreated }) => {
+  const drawingOptions = {
+    polyline: {
+      shapeOptions: {
+        color: "blue",
+        fillOpacity: 0.2,
+      },
+      icon: <FaDrawPolygon />,
+    },
+    rectangle: {
+      shapeOptions: {
+        color: "green",
+        fillOpacity: 0.2,
+      },
+      icon: <FaRegSquare />,
+    },
+    circle: {
+      shapeOptions: {
+        color: "red",
+        fillOpacity: 0.2,
+      },
+      icon: <FaRegCircle />,
+    },
+  };
+
+  return (
+    <EditControl
+      position="topright"
+      onCreated={onCreated}
+      draw={drawingOptions}
+    />
+  );
+};
 
 function MapComponent({ cHeight, cWidth }) {
   const [dimensions, setDimensions] = useState({});
   const [factor, setFactor] = useState(1);
-  const imageUrl =
-    "https://thumb.bibliocad.com/images/content/00020000/5000/25945.jpg";
+
+  const imageUrl = "https://thumb.bibliocad.com/images/content/00020000/5000/25945.jpg";
 
   useEffect(() => {
     Image.getSize(
@@ -30,9 +65,7 @@ function MapComponent({ cHeight, cWidth }) {
     [dimensions.height * factor, dimensions.width * factor],
   ];
 
-  // ------------------------------------------
-
-  var zones = [
+  const zones = [
     [
       [dimensions.height - 254, 679],
       [dimensions.height - 146, 679],
@@ -41,7 +74,7 @@ function MapComponent({ cHeight, cWidth }) {
     ],
   ];
 
-  var circles = [
+  const circles = [
     {
       center: [dimensions.height - 372, 659],
       radius: 20,
@@ -52,10 +85,13 @@ function MapComponent({ cHeight, cWidth }) {
     alert("Rectangle clicked!");
   };
 
-  // ------------------------------------------
+  const _created = (e) => {
+    const { layerType, layer } = e;
+    console.log(`Created ${layerType} with coordinates: ${layer.getLatLngs()}`);
+  };
 
-  zones = multiplyPositions(zones, factor);
-  circles = multiplyCenters(circles, factor);
+  zones.forEach((zone) => multiplyPositions(zone, factor));
+  circles.forEach((circle) => multiplyCenters(circle, factor));
 
   return (
     dimensions.height != null && (
@@ -72,49 +108,51 @@ function MapComponent({ cHeight, cWidth }) {
         attributionControl={false}
       >
         <ImageOverlay url={imageUrl} bounds={imageBounds} />
-        {zones.map((zone, index) => {
-          return (
-            <Polygon
-              key={index}
-              positions={zone}
-              stroke={false}
-              pathOptions={{
-                fillOpacity: 0.2,
-              }}
-              eventHandlers={{ click: onClickedZone }}
-            />
-          );
-        })}
-        {circles.map((circle, index) => {
-          return (
-            <Circle
-              key={index}
-              center={circle.center}
-              radius={circle.radius}
-              stroke={false}
-              pathOptions={{
-                fillOpacity: 0.2,
-              }}
-              eventHandlers={{ click: onClickedZone }}
-            />
-          );
-        })}
+        <FeatureGroup>
+          <DrawControls onCreated={_created} />
+          <FeatureGroup
+            onLayerClick={(e) => {
+              alert(`Clicked on ${e.layerType}`);
+            }}
+          />
+        </FeatureGroup>
+        {zones.map((zone, index) => (
+          <Polygon
+            key={index}
+            positions={zone}
+            stroke={false}
+            pathOptions={{
+              fillOpacity: 0.2,
+            }}
+            eventHandlers={{ click: onClickedZone }}
+          />
+        ))}
+        {circles.map((circle, index) => (
+          <Circle
+            key={index}
+            center={circle.center}
+            radius={circle.radius}
+            stroke={false}
+            pathOptions={{
+              fillOpacity: 0.2,
+            }}
+            eventHandlers={{ click: onClickedZone }}
+          />
+        ))}
       </MapContainer>
     )
   );
 }
 
-function multiplyPositions(zones, factor) {
-  return zones.map((zone) =>
-    zone.map((position) => position.map((coordinate) => coordinate * factor))
-  );
+function multiplyPositions(zone, factor) {
+  return zone.map((position) => position.map((coordinate) => coordinate * factor));
 }
 
-function multiplyCenters(circles, factor) {
-  return circles.map((circle) => ({
+function multiplyCenters(circle, factor) {
+  return {
     radius: circle.radius * factor,
     center: circle.center.map((coordinate) => coordinate * factor),
-  }));
+  };
 }
 
 export default MapComponent;
